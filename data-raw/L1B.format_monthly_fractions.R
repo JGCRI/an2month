aed027be84420c7b555774285023740e03e35d3a
dorh012 <kalyn.dorheim@pnnl.com>
2019-01-07 16:36:08 -0500

    add code to handle problem with precip in the Saharah

--- data-raw/L1B.format_monthly_fractions.R
+++ data-raw/L1B.format_monthly_fractions.R
@@ -1,115 +1,114 @@
 
-# Purpose: Format the monthly fractino netcdfs from L1A into a list of the time, girdcell corrdinate information, and the data frame of
-# month by grid cell monthly fraction.
-
-# TODO this script will need to be rewritten to be more flexible but due to time constraints we are only going to process one of the
-# isimip rcp monthly fractions since we proved that the rcp monthly fractions are essentially the same. Will also want to look into
-# how to document data....
-
-# TODO this script uses a mapping file to replace the expected NAs in the Sahara wtih 1/12.The mapping file is hard coded.
+# Purpose: Format the monthly fraction netcdfs from L1A into an array[nyear, nmonth, ngrid].
 
 # 0. Set Up -----------------------------------------------------------------------------------
 
 # Set up the directories
-BASE <- getwd() # should be the project location
+BASE   <- '/pic/projects/GCAM/Dorheim/grand_exp/an2month' # should be the project location
 INPUT  <- file.path(BASE, 'data-raw', 'output-L1')
-OUTPUT <- file.path(BASE, 'inst', 'extdata')
+OUTPUT <- file.path(BASE, 'data-raw', 'output-L2'); dir.create(OUTPUT)
 
 # Load the libraries
 library(dplyr)
 library(tidyr)
-library(netcdf)
 library(tibble)
+library(ncdf4)
+
+
+# 1. Mapping Information --------------------------------------------------------------
+
+# Find the netcdfs files on the INPUT
+netcdfs <- list.files(INPUT, pattern = '.nc', full.names = TRUE)
 
 # Import the missing cells mapping file
 NA_mapping <- read.table('C:/Users/dorh012/Documents/an2month/data-raw/mapping/missing_cells_mapping.tsv')
 
-# 1. Import netcdfs -----------------------------------------------------------------------------------
+# 2. Define functions ------------------------------------------------------------
 
-# the pr netcdf
-pr_nc_path <- list.files(INPUT, 'pr_ipsl-cm5a-lr_rcp2p6_avgFrac.nc', full.names = TRUE)
-pr_nc      <- nc_open(pr_nc_path)
+# This function imports netcdfs and formats the data as an array [year, month, gridcells]
+# Args:
+#     path - the input path
+#     var - the name of the variable
+#     output_dir - the name dir to save the formated data
+format_data <- function(input_path, var, output_dir){
 
-# the tas netcdf
-tas_nc_path <- list.files(INPUT, 'tas_ipsl-cm5a-lr_rcp2p6_avgFrac.nc', full.names = TRUE)
-tas_nc      <- nc_open(tas_nc_path)
+  # Import the nc
+  nc   <- nc_open(input_path)
 
+  # Extract data from  the nc
+  data <- ncvar_get(nc, var)
+  time <- ncvar_get(nc, 'time')
+  lon  <- ncvar_get(nc, 'lon')
+  lat  <- ncvar_get(nc, 'lat')
 
-# Extract the pr, tas, time, lat and lon data.
-pr_data  <- ncvar_get(pr_nc, 'pr')
-tas_data <- ncvar_get(tas_nc, 'tas')
-time     <- ncvar_get(tas_nc, 'time')
-lon      <- ncvar_get(tas_nc, 'lon')
-lat      <- ncvar_get(tas_nc, 'lat')
+  # Extract the dim
+  nlat  <- length(lat)
+  nlon  <- length(lon)
+  ntime <- length(time)
+  ngrid <- nlat * nlon
 
+  # Calculate the number of years in the data set
+  nyears <- ntime / 12
 
-# 2. Flatten the 3d array -----------------------------------------------------------------------------------
+  # Transpose the array
+  data_t <- aperm(data, c(3, 2, 1))
 
-# This is the same method that is used by the fldgen to flatten the 3d array with lat as the most rapidly
-# varying dimension.
+  # Flatten the matrix by the grid cells
+  dim(data_t) <- c(ntime, ngrid)
 
-# First save the dim of lat, lon, and time.
-nlat  <- length(lat)
-nlon  <- length(lon)
-ntime <- length(time)
+  # Format the 2d matrix by year, 12, gridcell
+  dim(data_t) <- c(nyears, 12, ngrid)
 
-# Format the fractions
-pr_frac      <- aperm(pr_data, c(3,2,1))
-dim(pr_frac) <- c(ntime, nlat*nlon)
-pr_frac      <- as.matrix(pr_frac)
+  # Add names to the array
+  dimnames(data_t)[[1]] <- unique(substr(time, 1, 4))
+  dimnames(data_t)[[2]] <- c("January", "February", "March", "April", "May", "June", "July", "August",
+                             "September", "October", "November", "December")
 
-tas_frac      <- aperm(tas_data, c(3,2,1))
-dim(tas_frac) <- c(ntime, nlat*nlon)
-tas_frac      <- as.matrix(tas_frac)
+  # Only for the pr in the Sharah desert replace the monthly fractions with 1/12 to avoid
+  # problems latter on. This problem does not occur with temp.
+  if(var == 'pr'){
 
+    # Format the grid cell coordinat data frame.
+    tibble(lat = lat) %>%
+      mutate(join = 1) ->
+      lat_tibble
 
-# 3. Format the time and corrdinates list -----------------------------------------------------------------------------
+    tibble::tibble(lon = lon) %>%
+      dplyr::mutate(join = 1) %>%
+      dplyr::left_join(lat_tibble, by = "join") %>%
+      dplyr::mutate(column_index = 1:(nlat * nlon)) %>%
+      dplyr::select(column_index, lat, lon) ->
+      coordinates
 
-# Format the grid cell coordinat data frame.
-tibble(lat = lat) %>%
-  mutate(join = 1) ->
-  lat_tibble
+    # Use the coordinates and the NA cells mapping file to repalce the NAs in the pr_frac with 1/12.
+    NA_mapping %>%
+      select(lat, lon) %>%
+      mutate(keep = 1) %>%
+      full_join(coordinates,  by = c("lat", "lon")) %>%
+      na.omit ->
+      replace
 
-tibble::tibble(lon = lon) %>%
-  dplyr::mutate(join = 1) %>%
-  dplyr::left_join(lat_tibble, by = "join") %>%
-  dplyr::mutate(column_index = 1:(nlat * nlon)) %>%
-  dplyr::select(column_index, lat, lon) ->
-  coordinates
+    # Replace NAs with 1/12
+    data_t[ , replace$column_index] <- 1/12
 
-# Use the coordinates and the NA cells mapping file to repalce the NAs in the pr_frac with 1/12.
-NA_mapping %>%
-  select(lat, lon) %>%
-  mutate(keep = 1) %>%
-  full_join(coordinates,  by = c("lat", "lon")) %>%
-  na.omit ->
-  replace
+  }
 
-# Replace NAs with 1/12
-pr_frac[ , replace$column_index] <- 1/12
+  # Save the array as an rds
+  saveRDS(data_t, file = file.path(output_dir, gsub(basename(input_path), pattern = '.nc', replacement = '.rds')))
 
-
-# Check to make sure that the number of gridcells in the coordinates mapping file matches the number of
-# grid cells in the fraction matrix.
-if(nrow(coordinates) != ncol(tas_frac)) stop('Unxpected number of rows in the coordinates mapping file')
-
-
-# Add the month name to the time
-month <- substr(start = 5, stop = 6, x = time)
-month_name <- c("January", "February", "March", "April", "May", "June", "July", "August",
-                "September", "October", "November", "December")
-time_mapping <- tibble(month = month, month_name = month_name)
-
+}
 
 
-# 3. Save the output as package data ----------------------------------------------------------------------------
-
-frac_ipsl_cm5a_lr <- list(tas = tas_frac, pr = pr_frac, coordinates = coordinates, time = time_mapping)
-
-devtools::use_data(frac_ipsl_cm5a_lr, overwrite = TRUE)
 
 
-# End
-# Check the fractions to make sure that fractions look good
-# apply(pr_frac, MARGIN = 2, sum) -> check_pr
-# apply(tas_frac, MARGIN = 2, sum) -> check_tas
+# 3. Format the data ---------------------------------------------------------------------------
+tibble(path = netcdfs,
+       file = basename(path)) %>%
+  tidyr::separate(file,
+                  into = c('var', 'model', 'exp', 'frac'),
+                  remove = FALSE,
+                  sep = '_') %>%
+  split(.$path) %>%
+  lapply(function(input){format_data(input_path = input$path, var = input$var, output_dir = OUTPUT)})
+
+
